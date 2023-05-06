242735c88fa03103b17455af8a7d63d24dc29a06
Thallys <thallys_souza@sicredi.com.br>
2023-05-06 16:49:03 -0300

    decimal to binary conversion

--- src/teste/Aula24.java
+++ src/teste/Aula24.java
@@ -5,13 +5,36 @@ import java.util.Stack;
 public class Aula24 {
 
     public static void main(String[] args) {
+        System.out.println(convertBinaryToDecimal(10));
+        System.out.println(convertBinaryToDecimal(45));
+        System.out.println(convertBinaryToDecimal(59));
+        System.out.println(convertBinaryToDecimal(101));
+        System.out.println(convertBinaryToDecimal(2));
+
 
     }
 
-    public static boolean verificaExpressao(String expressao) {
-        Stack<Character> stack = new Stack<>();
+    public static Stack<Integer> convertBinaryToDecimal(Integer value) {
+        Stack<Integer> stack = new Stack<>();
+        while (value != 0) {
+            if(value % 2 == 0){
+                stack.push(0);
+            }else {
+                stack.push(1);
+            }
+            value /= 2;
+        }
 
-        return false;
+        return inverseOrder(stack);
     }
 
+    public static Stack<Integer> inverseOrder(Stack<Integer> binary){
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = binary.size()-1; i > -1; i--) {
+            stack.push(binary.get(i));
+        }
+        return stack;
     }
+
+}
